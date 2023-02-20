cebd658c039ee3f784b8ad8b9790d89a543602af
Dato Kobaidze <kobaidze.dato00@gmail.com>
2023-02-20 21:39:49 +0400

    First condition

--- SA_15.py
+++ SA_15.py
@@ -1,2 +1,10 @@
 
 
+A = int(input('A = '))          
+B = int(input('B = '))
+
+
+if A < B:                       
+    while (A+3) < B:            
+        print(A+3)              
+        A += 3
